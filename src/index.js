097b5b310dfaa7570b3d14df21fa1361678f1f64
Alberto <acdevhub@gmail.com>
2021-11-26 12:33:10 +0100

    Finishing app endpoints + css styling start 26/11

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 require("dotenv").config();
 const userRoutes = require("./routes/user");
+const methodOverride = require("method-override");
 const app = express();
-const ObjectId = require("mongodb").ObjectID;
 const port = process.env.PORT || 9000;
 
 // middleware
@@ -18,12 +18,14 @@ app.use(express.json());
 app.use("/api", userRoutes);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 MongoClient.connect(process.env.MONGODB_URI)
   .then((client) => {
     const db = client.db("myFirstDatabase");
     const usersCollection = db.collection("users");
-    // CREAR USUARIOS
+
+    // CREAR CONTACTOS
     app.post("/users", (req, res) => {
       usersCollection
         .insertOne(req.body)
@@ -33,6 +35,8 @@ MongoClient.connect(process.env.MONGODB_URI)
         })
         .catch((error) => console.error(error));
     });
+
+    // BORRAR CONTACTOS
     app.post("/delete", (req, res) => {
       usersCollection
         .deleteOne({ _id: mongoose.Types.ObjectId(req.body.id) })
@@ -52,7 +56,43 @@ MongoClient.connect(process.env.MONGODB_URI)
         .then((results) => {
           res.render("index", { users: results });
         })
-        .catch(/* ... */);
+        .catch((error) => console.error(error));
+    });
+
+    // Obtener página edit
+
+    app.post("/edit", (req, res) => {
+      usersCollection
+        .findOne({ _id: mongoose.Types.ObjectId(req.body.id) })
+        .then((results) => {
+          console.log(results);
+          res.render("edit", { users: results });
+        })
+        .catch((error) => console.error(error));
+    });
+
+    // Editar el contacto
+
+    app.put("/edit", (req, res) => {
+      usersCollection
+        .findOneAndUpdate(
+          { _id: mongoose.Types.ObjectId(req.body.id) },
+          {
+            $set: {
+              name: req.body.name,
+              surname: req.body.surname,
+              phone: req.body.phone,
+              email: req.body.email,
+            },
+          },
+          { new: true, omitUndefined: true }
+        )
+        .then((result) => {
+          console.log(result);
+          console.log(req.body);
+        })
+        .catch((error) => console.error(error))
+        .catch((error) => console.error(error));
     });
   })
   .catch(console.error);
